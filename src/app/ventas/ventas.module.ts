919a07abdb86b166b470977500c4e9be67a813cd
Bryan <bryan.zamora@goitsa.me>
2020-09-28 17:32:05 -0500

    Estructura del proyecto

---
+++ src/app/ventas/ventas.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VentasRoutingModule } from './ventas-routing.module';
+import { LoginComponent } from './login/login.component';
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    VentasRoutingModule
+  ]
+})
+export class VentasModule { }
