f11cf6456d81b86b323675cc4740a69fc34db76d
Bryan <bryan.zamora@goitsa.me>
2021-02-14 14:12:04 -0500

    Se agrega un menu por implementar

--- src/app/ventas/home/usuarios/prueba/prueba.component.ts
+++ src/app/ventas/home/usuarios/prueba/prueba.component.ts
@@ -12,4 +12,13 @@ export class PruebaComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
 }
