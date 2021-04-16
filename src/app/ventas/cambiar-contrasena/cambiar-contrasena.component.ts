8cad9358568d3f8665d9eb5ad9cc8757e30837a4
Bryan <bryan.zamora@goitsa.me>
2021-04-15 21:18:09 -0500

    Se agrega fincionales al componente de cambio de contraseña

--- src/app/ventas/cambiar-contrasena/cambiar-contrasena.component.ts
+++ src/app/ventas/cambiar-contrasena/cambiar-contrasena.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-cambiar-contrasena',
@@ -12,17 +13,42 @@ export class CambiarContrasenaComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  typeInputF="password";
-  showPF:boolean=true;
-  contrasena:any="";
-  mostrarPassword(){
+  typeInputF1="password";
+  showPF1:boolean=true;
+  contrasena1:any="";
+  mostrarPassword1(){
 
-    if(this.typeInputF=="text"){
-      this.showPF=true;
-      this.typeInputF="password";
+    if(this.typeInputF1=="text"){
+      this.showPF1=true;
+      this.typeInputF1="password";
     }else{
-      this.showPF=false;
-      this.typeInputF="text";
+      this.showPF1=false;
+      this.typeInputF1="text";
+    }
+  }
+
+  typeInputF2="password";
+  showPF2:boolean=true;
+  contrasena2:any="";
+  mostrarPassword2(){
+
+    if(this.typeInputF2=="text"){
+      this.showPF2=true;
+      this.typeInputF2="password";
+    }else{
+      this.showPF2=false;
+      this.typeInputF2="text";
+    }
+  }
+
+  compararContrasena(){
+    if(this.contrasena2!="" && this.contrasena1!=""
+     && this.contrasena2==this.contrasena1){
+      console.log("Si");
+      $("#aceptar").prop('disabled', false);
+    }else{
+      console.log("No");
+      $("#aceptar").prop('disabled', true);
     }
   }
 
