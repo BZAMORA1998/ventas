524bd74d00fce785ee405bfa2d501491c05a6bb7
Bryan <bryan.zamora@goitsa.me>
2021-05-07 10:04:47 -0500

    Se ajusta rutas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrlSpring:"https://sistema-ventas-api.herokuapp.com/ventas/v1",
+  apiUrlSpring:"https://sistemas-ventas-api.herokuapp.com/ventas/v1",
   apiUrlNodeJS:"https://com-general-api.herokuapp.com",
   //token:"Bearer "+JSON.parse(localStorage.getItem("token")),
   languaje:"es"
